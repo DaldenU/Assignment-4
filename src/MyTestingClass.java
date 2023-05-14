c913ad725a76e72f17272692369216bfecd56661
DaldenU <daldenuteryag@gmail.com>
2023-05-14 20:13:27 +0600

    MyTestingClass implementation

---
+++ src/MyTestingClass.java
@@ -0,0 +1,27 @@
+public class MyTestingClass {
+    private int x;
+    private int y;
+
+    public MyTestingClass(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    // Custom hashcode method
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + x;
+        result = prime * result + y;
+        return result;
+    }
+}
